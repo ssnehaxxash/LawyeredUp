a46ab3dc614f7238d5f19aaac4f302deea5cc1d5
Sneha Pathak <snehasunilpathak@gmail.com>
2025-10-16 22:00:31 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,33 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+};
+
+export default nextConfig;
